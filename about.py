51b4aa05ff5f7f153f8d1b99d5aa8f3b47c73d85
halimkun <ffaisalhalim@gmail.com>
2022-12-03 11:29:58 +0700

    :sparkles: tfjs init

--- about.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from config import Config
-
-class About:
-    def __init__(self):
-        self.option = Config().get_option()
-    
-    def main(self):
-        st.title("About")
